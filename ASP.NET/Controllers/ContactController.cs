ca705aa797ffc5e21b06518460685c51031f3828
verhovensky <verhovenskyivan@gmail.com>
2022-10-20 22:47:07 +0300

    added new form

---
+++ ASP.NET/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult About()                    
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace ASP.NET.Models
+{
+	public class Contact
+	{
+		public string Name { get; set; }	
+
+		public string Email { get; set; }	
+
+
+		public DateTime CreatedDate { get; set; }
+		
+		public string Message { get; set; }	
+	}
+}
